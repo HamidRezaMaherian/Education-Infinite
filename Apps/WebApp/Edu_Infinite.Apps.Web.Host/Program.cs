fb6e7f89f1c71889250c9c3328ca95e6e056cce4
Hamid RM <hamidali.reza41@outlook.com>
2024-04-05 01:17:10 +0330

    added web host project

---
+++ Apps/WebApp/Edu_Infinite.Apps.Web.Host/Program.cs
@@ -0,0 +1,82 @@
+
+namespace Edu_Infinite.Apps.Web.Host
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         var builder = WebApplication.CreateBuilder(args);
+
+         // Add services to the container.
+         builder.Services.AddRazorPages();
+         builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+         builder.Services.ConfigureServices();
+         builder.Services.AddBff();
+
+         builder.Services.AddAuthentication(options =>
+         {
+            options.DefaultScheme = "cookie";
+            options.DefaultChallengeScheme = "oidc";
+            options.DefaultSignOutScheme = "oidc";
+         })
+             .AddCookie("cookie", options =>
+             {
+                options.Cookie.Name = "__Host-blazor";
+                options.Cookie.SameSite = SameSiteMode.Strict;
+             })
+             .AddOpenIdConnect("oidc", options =>
+             {
+                options.Authority = "https://demo.duendesoftware.com";
+
+                // confidential client using code flow + PKCE
+                options.ClientId = "interactive.confidential";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+
+                options.MapInboundClaims = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+
+                // request scopes + refresh tokens
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("api");
+                options.Scope.Add("offline_access");
+             });
+
+         var app = builder.Build();
+
+         // Configure the HTTP request pipeline.
+         if (!app.Environment.IsDevelopment())
+         {
+            //app.UseWebAssemblyDebugging();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+         }
+         app.UseHttpsRedirection();
+         app.UseBlazorFrameworkFiles();
+         app.UseStaticFiles();
+
+         app.UseRouting();
+
+         app.UseAuthentication();
+         app.UseBff();
+         app.UseAuthorization();
+
+         app.MapBffManagementEndpoints();
+         app.MapRazorPages();
+
+         app.MapControllers()
+             .RequireAuthorization()
+             .AsBffApiEndpoint();
+
+         //app.MapFallbackToFile("index.html");
+         app.MapFallbackToPage("/_Host");
+
+         app.Run();
+      }
+   }
+}
--- Apps/WebApp/Edu_Infinite.Apps.Web/Program.cs
+++ Apps/WebApp/Edu_Infinite.Apps.Web/Program.cs
@@ -1,20 +1,23 @@
 using Edu_Infinite.Apps.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Runtime.CompilerServices;
 
 namespace Edu_Infinite.Apps.Web
 {
-   public class Program
+   public static class Program
    {
       public static async Task Main(string[] args)
       {
          var builder = WebAssemblyHostBuilder.CreateDefault(args);
-         builder.RootComponents.Add<App>("#app");
-         builder.RootComponents.Add<HeadOutlet>("head::after");
-         builder.Services.AddSingleton<CourseClientService>();
-         builder.Services.AddBlazorBootstrap();
-
+         //builder.RootComponents.Add<App>("#app");
+         builder.Services.ConfigureServices();
          await builder.Build().RunAsync();
+      }
+      public static void ConfigureServices(this IServiceCollection services)
+      {
+         services.AddSingleton<CourseClientService>();
+         services.AddBlazorBootstrap();
       }
    }
 }
